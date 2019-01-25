be6b9ba0ac8215438d2f2c5895d012741bebddfe
csemmel524 <46930048+csemmel524@users.noreply.github.com>
2019-01-25 14:29:39 -0500

    exercise 2 fixed issue two

--- src/common/Functions.js
+++ src/common/Functions.js
@@ -2,7 +2,20 @@
 
 // Converts camel case string to a sentence case
 export const formatCamelCaseString = text => {
-  return text;
+  //function found at 
+  //https://stackoverflow.com/questions/7225407/convert-camelcasetext-to-sentence-case-text
+  function camelCaseToTitle(camelCase) {
+    if (!camelCase) {
+        return '';
+    }
+    var pascalCase = camelCase.charAt(0).toUpperCase() + camelCase.substr(1);
+    return pascalCase
+        .replace(/([a-z])([A-Z])/g, '$1 $2')
+        .replace(/([A-Z])([A-Z][a-z])/g, '$1 $2')
+        .replace(/([a-z])([0-9])/gi, '$1 $2')
+        .replace(/([0-9])([a-z])/gi, '$1 $2');
+  }
+  return camelCaseToTitle(text);
 };
 
 // Handles page navigation
